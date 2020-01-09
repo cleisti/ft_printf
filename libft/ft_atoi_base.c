9cf1cbeb268092496a9256cbf32ae90c83cabd7b
Camilla Leisti <camilla@88-114-99-36.elisa-laajakaista.fi>
2020-01-09 22:01:43 +0200

    atoi base

---
+++ libft/ft_atoi_base.c
@@ -0,0 +1,46 @@
+#include "libft.h"
+
+int		check_validity(char c, int str_base)
+{
+	char	*l_valids = "0123456789abcdef";
+	char	*u_valids = "0123456789ABCDEF";
+
+	while (str_base)
+	{
+		if (c == l_valids[str_base] || c == u_valids[str_base])
+			return (1);
+		str_base--;
+	}
+	return (0);
+}
+
+int		convert_base(char c, int str_base)
+{
+	if (c >= '0' && c <= '9')
+		return (c - '0');
+	else if (c >= 'a' && c <= 'f')
+		return ((c - 'a') + 10);
+	return ((c - 'A') + 10);
+}
+
+int		ft_atoi_base(const char *str, int str_base)
+{
+	unsigned long long	res;
+	int					neg;
+	int					i;
+
+	res = 0;
+	neg = 1;
+	i = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	(str[i] == '-' || str[i] == '+') ? i++ : 0;
+	if (str[i - 1] == '-')
+		neg = -1;
+	while (check_validity(str[i], str_base))
+	{
+		res = res * str_base + convert_base(str[i], str_base);
+		i++;
+	}
+	return (neg * res);
+}
